fadd53d4baa2ba80c41e30195b93124f049cab39
mycolo1983 <13668993@qq.com>
2021-10-07 23:23:58 +0800

    Update httpserver.go

--- httpserver.go
+++ httpserver.go
@@ -19,8 +19,18 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "Hello Web, %s!", r.Form.Get("name")) //5 写入到 w 的是输出到客户端的内容
 }
 
+func write(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Custom-Header", "custom") // 设置自定义的头部
+	w.WriteHeader(201) // 设置创建用户的状态码
+}
+
+
+
+
 func main() {
 	http.HandleFunc("/", sayHello)           //1 设置访问的路由
+	http.HandleFunc("/write", write) 
 	err := http.ListenAndServe(":8080", nil) //2 设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
